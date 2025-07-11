92bbf023a9cc6f762ff72a7f985e60c95b7bc818
Sisalem-Amine <sisamine0@gmail.com>
2025-07-11 14:49:22 +0100

    database added and users load

--- app.py
+++ app.py
@@ -1,9 +1,45 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
+app.secret_key = 'secret_key'
+
+login_manager = LoginManager()
+login_manager.login_view = "login"
+login_manager.init_app(app)
+
+class User(UserMixin):
+    def __init__(self, id, username):
+        self.id = id
+        self.username = username
+
+    def get_id(self):
+        return str(self.id)
+
+@login_manager.user_loader
+def load_user(user_id):
+    conn = sqlite3.connect("users.db")
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users WHERE id = ?", (user_id,))
+    row = cur.fetchone()
+    conn.close()
+
+    if row:
+        return User(id=row["id"], username=row["username"])
+    return None
 
 @app.route("/")
 def index():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("index.html")
+
+@app.route("/register")
+def register():
     return render_template("index.html")
 
 if __name__ == "__main__":
